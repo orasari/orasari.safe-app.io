8dee7c636d4cf469a554588fa7a975e3a62cf48a
orasari <stankovicd.mina@gmail.com>
2019-03-28 20:29:42 +0100

    Updates

---
+++ precache-manifest.a7f52bda89529c471a625b73dc7d695d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c9fde0a879743447874",
+    "url": "/orasari.safe-app.io/static/js/runtime~main.958d2aa1.js"
+  },
+  {
+    "revision": "79fdae8b7cbb8e268f01",
+    "url": "/orasari.safe-app.io/static/js/main.8ce7cbc3.chunk.js"
+  },
+  {
+    "revision": "aa6f23b0bfd055bd4548",
+    "url": "/orasari.safe-app.io/static/js/2.a656ced5.chunk.js"
+  },
+  {
+    "revision": "79fdae8b7cbb8e268f01",
+    "url": "/orasari.safe-app.io/static/css/main.2a8dedc2.chunk.css"
+  },
+  {
+    "revision": "c7d74a80443e0c9c53d797f988796ea2",
+    "url": "/orasari.safe-app.io/index.html"
+  }
+];
