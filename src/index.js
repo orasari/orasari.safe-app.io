7c943f5008a598a5368078228d368ca4e109bd0e
orasari <stankovicd.mina@gmail.com>
2019-03-28 21:08:45 +0100

    browser router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+<App />
+</BrowserRouter>, document.getElementById('root'));
 
