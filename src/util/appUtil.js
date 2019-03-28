50298a8aacbbb3610548bcf6c1eb7a8b7533ceb5
orasari <stankovicd.mina@gmail.com>
2019-03-28 21:51:15 +0100

    fix before deploy

--- src/util/appUtil.js
+++ src/util/appUtil.js
@@ -1,8 +1,8 @@
-const password = JSON.parse(localStorage.getItem('password'))
+
 export const serialNumber = {sn: 4815162342}
 
 export const passwordExists = () => {
-    var pass = localStorage.getItem('password')
+    let pass = localStorage.getItem('password')
     if(pass) {
         return true;
     }else return false
@@ -34,12 +34,24 @@ export const setPassword = (enteredPass) => {
 }
 
 export const validatePassword = (enteredPass) =>{
-    // console.log(enteredPass)
+    let password = JSON.parse(localStorage.getItem('password'))
     let validPass = password+''
     let validan = false;
+    console.log("val enteredPass",enteredPass)
+    console.log("val password",validPass)
+    
+    console.log("password password",password)
+    console.log("leng 1 ",validPass.length)
+    console.log("leng 2 ",enteredPass.length)
+    console.log("leng 31 ",validPass.length === enteredPass.length)
+    console.log("leng 4 ",validPass.length == enteredPass.length)
+
     if(validPass.length === enteredPass.length) {
         for (let i = 0; i < enteredPass.length; i++) {
             if (enteredPass[i] === validPass[i]) {
+                console.log("enteredPass ",enteredPass)
+                console.log("validPass ",validPass)
+                console.log("oba ",enteredPass[i] === validPass[i])
                 validan = true
             } else {
                 validan = false
